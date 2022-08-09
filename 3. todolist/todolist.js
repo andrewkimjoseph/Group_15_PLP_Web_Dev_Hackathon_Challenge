7061743ea51fe79cd6cb70d5b70d6d6d10b4d4e5
andrewkimjoseph <91619206+andrewkimjoseph@users.noreply.github.com>
2022-08-09 12:27:21 +0300

    IMPROVING THE FILES

--- 3. todolist/todolist.js
+++ 3. todolist/todolist.js
@@ -1,50 +1,39 @@
-let allTasks = [];
-
-tasksAdded = 0;
-
-function taskClicked(){
-    alert('Task Clicked!')
-}
-
 function mainEvent(){
+    // THIS IS THE MAIN EVENT FUNCTION
+    // IT DOES THE FOLLOWING:
+    // 1. IT TAKES IN THE TASK ENTERED AND ADDS IT TO THE VIEW TASK SECTION
+    // 2. IT CREATES USEFUL BUTTONS TO HELP EDIT (UPDATE), REMOVE, OR MARK A TASK AS COMPLETE
+
+    // ----------- SECTION 1: TAKING IN THE TASK ENTERED AND ADDING IT TO THE VIEW TASK SECTION ------------- 
     let taskTitle = document.getElementById('title').value;  //GETTING THE TASK TITLE
     let taskDetails = document.getElementById('actual').value; //GETTING THE ACTUAL TASK DETAILS
     let taskDate = document.getElementById('taskdate').value; //GETTING THE TASK DATE
 
     // CHECKING IF THE USER HAS ENTERED SOMETHING VALID, AND THAT THE INPUT FIELDS ARE NOT BLANK
-    if (taskDate === "" || taskTitle === "" || taskDate === ""){
+    if (taskTitle === "" || taskDetails === "" || taskDate === ""){
         alert("One or Task Details Missing, Cannot Be Left Blank");
     } else {
         //OUR WAY OF PRESENTING THE TASK ON THE BROWSER USING THE DETAILS ENTERED
         newTask = taskTitle + '  |  ' + taskDetails + '  |  ' + taskDate;
-        
         // OUR TASK GOES INTO A BUTTON
         let taskButton = document.createElement("p");
         // SOME LITTLE STYLING
         taskButton.style.marginBottom = "0";
-
         taskButton.textContent = newTask;
         // EACH TIME A TASK IS CREATED, THREE BUTTONS ARE CREATED ALONG WITH IT
         // 1. THE MARK COMPLETED TASK BUTTON ✅
         let completeButton = document.createElement("button");
         completeButton.textContent = "✅ Done";
-        
         // 2. THE EDIT TASK BUTTON ✍️
         let editButton = document.createElement("button");
         editButton.textContent = "✍️ Edit";
-        
         // 3. THE DELETE TASK BUTTON 🗑️ 
         let deleteButton = document.createElement("button");
         deleteButton.textContent = "🗑️ Delete";
-
-        //WE NEED A BREAKLINE TO SEPARATE THE TASK AND THE BUTTONS
-        let breakLine = document.createElement('br');
         // WE NEED A DIV TO STORE THE TASK AND THE BUTTONS
         // THEN WE'LL REFERENCE IT WHEN TRYING TO DELETE THE TASK
         let taskDiv = document.createElement('div'); // WE CREATE A DIV TO STORE THE TASK
-  
         let buttonsDiv = document.createElement('div'); //ANOTHER DIV FOR THE BUTTONS
-
         let containerDiv = document.createElement('div'); //WILL CARY BOTH DIVS ABOVE
 
         //THROWING THE TASK INTO THE TASK DIV
@@ -53,47 +42,58 @@ function mainEvent(){
         buttonsDiv.appendChild(completeButton)
         buttonsDiv.appendChild(editButton);
         buttonsDiv.appendChild(deleteButton);
-
         // PUTTING BOTH DIVS THAT CONTAIN TASK AND BUTTON
         containerDiv.appendChild(taskDiv);
         containerDiv.appendChild(buttonsDiv);
-
         // GETTING THE VIEW TASK AREA 
         newTaskArea = document.getElementById('pendingTasks');
         // THROWING THE CONTAINER DIV THERE
         newTaskArea.appendChild(containerDiv);
-        // ADDING FUNCTIONALITY TO EACH BUTTON GIVEN FOR THE TASK
-        //1. THE COMPLETE BUTTON
+
+        // CLEARING INPUT BOXES TO ADD THE NEXT TASK
+        // GETTING THE CURRENT INPUT FILES AGAIN
+        let task = document.getElementById('title');
+        let details = document.getElementById('actual');
+        let date = document.getElementById('taskdate');
+        task.value = "";
+        details.value = "";
+        date.value = "";
 
         alert('Task Added');
+
+    // ----------- SECTION 2: CREATING USEFUL BUTTONS TO HELP EDIT (UPDATE), REMOVE, OR MARK A TASK AS COMPLETE ------------- 
+        // ALL THESE FUNCTIONS ARE TIED TO THE THREE BUTTONS WE CREATED
         completeButton.addEventListener("click", () => {
+            // HELPS TO MARK AN ITEM COMPLETE AND REMOVE IT FROM THE LIST
             completedTaskText = document.getElementById("pendingTasks").firstChild; // WE GET THE TEXT OF THE COMPLETED TASK
             completeTasksArea = document.getElementById("completeTasks"); // WE WILL POST THE COMPLETED TASK HERE ONCE IT IS MARKED COMPLETE
-            completeParagraph = document.createElement('p');
+            completeParagraph = document.createElement('p'); // WE CREATE A PARAGRAPH TO POST THE COMPLETED TASK
 
-            completedTaskText.removeChild(completedTaskText.lastChild)
-            console.log(completedTaskText.textContent);
-            completeParagraph.innerHTML = completedTaskText.textContent + ' ✅';
-            
-            completeTasksArea.appendChild(completeParagraph);
-            containerDiv.remove();
+            completedTaskText.removeChild(completedTaskText.lastChild) // WE NEED TO REMOVE ALL BUTTONS BEFORE WE POST THE TASK
+            // REMEMBER THAT THEY ARE ALL ONE UNIT
+            completeParagraph.innerHTML = completedTaskText.textContent + ' ✅'; //THE COMPLETED TASK WILL BE POSTED IN
+            // THE COMPLETED TASK AREA WITH A ✅ NEXT TO IT
+            containerDiv.remove(); // THE CONTAINER DIV WILL BE REMOVED FROM THE TASKS-AT-HAND AREA
+            completeTasksArea.appendChild(completeParagraph); // ADDING THE CLEAN COMPLETED TASK TO THE COMPLETE TASKS AREA
             alert('Task Done, Way to Go!')
         });
 
         deleteButton.addEventListener("click", () => {
+            // HELPS REMOVE AN ITEM FROM THE TASKS-AT-HAND AREA
             confirmMessage = 'Are You Sure You Want to Delete The Task? ';
             if (window.confirm(confirmMessage)){
-                containerDiv.remove();
+                containerDiv.remove(); // THE CONTAINER DIV WILL BE REMOVED FROM THE TASKS-AT-HAND AREA
             }
-            
         });
 
         editButton.addEventListener('click', function handleClick() {
+            // HELPS A USER TO EDIT THE TASK USING A BROWSER PROMPT
             let taskChanges = prompt('Edit the Task Here: ', newTask);
-
-            taskButton.textContent = taskChanges;
+            if(taskChanges){
+                taskButton.textContent = taskChanges;
+            }else{
+                taskChanges = newTask;
+            }
         });
-
-        
     }
 }
--- 4. atm/main.js
+++ 4. atm/main.js
@@ -1,40 +1,56 @@
-const defaultPassword = "0000";
-let startingBalance = 10000;
-let atmTransactions = [];
-let today = new Date();
+// GLOBAL VARIABLE TO STORE DIFFERENT, RELEVANT DATA
+const defaultPassword = "0000"; // FOR CONVENIENCE, WE'VE SET A DEFAULT PASSWORD THAT YOU CAN VIEW ON THE BROWSER
+let startingBalance = 10000; // OUR SYSTEM GIVES YOU KES. 10,000 AS THE STARTING VALUE
+let atmTransactions = []; // STORE ALL TRANSACTIONS AND IS USEFUL WHEN CREATING THE TRANSACTION RECEIPT FOR DOWNLOAD
+let today = new Date(); // AN INSTANCE OF THE DATE() OBJECT, FOR DATE/TIME INFO AS THE TASK REQUIRES
        
 function loggedIn(){
+    // WHEN THE LOG IN BUTTON IS CLICKED
+    // THIS FUNCTION IS TRIGGERED
     window.location.href = 'atm.html';
 }
 function loggedOut(){
+    // TRIGGERED WHEN EXIT PROCESS IS COMPLETE
+    // BASICALLY TAKES YOU BACK TO THE PASSWORD PAGE
     window.location.href = 'main.html';
 }
 function login(){
+    // CHECKS IF ENTERED PASSWORD MATCHES SAVED PASSWORD
     let enteredPassword = document.getElementById('password').value;
     if (enteredPassword == defaultPassword){
-        alert("Password Correct!");
-        return loggedIn();
+        alert("Password Entered Correctly, Click OK to Access System");
+        return loggedIn(); // IF SUCCESSFULLY, LOG IN HAPPENS
     } else {
         alert("Password Not Entered or Incorrect!");
-        enteredPassword.value = '';
+        enteredPassword.value = ''; // HELPS TO RESET THE VALUE TO BLANK FOR CLEANER INPUT FILES
     }
 }
 
 function showPassword(){
+    // FOR CONVENIENCE, OUR SYSTEM SHOWS YOU THE DEFAULT PASSWORD
+    // WHEN THIS BUTTON IS CLICKED
     let displayAreaPassword = document.getElementById('show-password');
-    let textNode = "HELLO, THE DEFAULT PASSWORD IS:  " + defaultPassword;
+    displayAreaPassword.style.backgroundColor = "#9ED2C6";
+    displayAreaPassword.style.fontSize = "1.5rem";
+    let textNode = "HELLO, THE DEFAULT PASSWORD IS: " + defaultPassword;
     displayAreaPassword.innerHTML = textNode;
 
 }
 
 function showBalance(){
+    // BUTTON THAT WRITES BALANCE IN THE #balance DIV
     let displayAreaBalance = document.getElementById('balance');
     let textNode = 'BALANCE: ' + startingBalance.toLocaleString("en-US");
     displayAreaBalance.innerHTML = textNode;
 }
 
 function withdraw(){
+    // TRIGGERED WHEN WITHDRAW IS CLICKED
     let moneyOut = parseInt(document.getElementById('moneyout').value);
+    // WE NEED TO CHECK IF:
+    // 1. THE VALUE ENTERED IS NAN - MEANING THAT THE USER DID NOT ENTER ANYTHING
+    // 2. THE VALUE ENTERED IS LESS THAN 0 - CANNOT WITHDRAW A NEGATIVE VALUE
+    // 3. THE VALUE ENTERED IS LESS THAN 1000 - OUR SYSTEM SETS A WITHDRAWAL LIMIT OF KES. 1000 PER WITHDRAWAL
     if (isNaN(moneyOut)){
         alert('You Need to Enter a Valid Figure');
     } else {
@@ -48,15 +64,19 @@ function withdraw(){
             alert('You Cannot Withdraw An Amount Larger Than Your Current Balance\n\nCurrent Balance is: ' + startingBalance.toLocaleString("en-US"));
             document.getElementById('moneyout').value = '';
         } else {
-            confirmMessage = 'Are You Sure You Want to Withdraw ' + moneyOut;
+            // IF ALL IS WELL
+            // A CONFIRMATION MESSAGE SHOWS
+            confirmMessage = 'Are You Sure You Want to Withdraw ' + moneyOut + '?';
             if (window.confirm(confirmMessage)){
-                startingBalance -= moneyOut;
+                startingBalance -= moneyOut; // WITHDRAWN AMOUNT IS DEDUCTED FROM BALANCE
                 alert('Withdrawal Successful');
                 transactionNote = 'TYPE: WITHDRAWAL, AMOUNT: ' + moneyOut;
+                // HERE, WE UPDATE THE atmTransactions ARRAY TO INCLUDE NEW TRANSACTION, LIKE THIS
+                // TYPE: WITHDRAWAL, AMOUNT: 10000
                 atmTransactions.push(transactionNote);
                 document.getElementById('moneyout').value = '';
                 let balanceButton = document.getElementById('balanceButton');
-                balanceButton.click();
+                balanceButton.click(); // BALANCE BUTTON CLICKED TO REFRESH BALANCE 
             } else {
                 alert("Operation Cancelled");
                 document.getElementById('moneyout').value = '';
@@ -67,12 +87,16 @@ function withdraw(){
 }
 
 function deposit(){
+    // TRIGGERED WHEN DEPOSIT IS CLICKED
     let moneyIn = parseInt(document.getElementById('moneyin').value);
+    // WE NEED TO CHECK IF:
+    // 1. THE VALUE ENTERED IS NAN - MEANING THAT THE USER DID NOT ENTER ANYTHING
+    // 2. THE VALUE ENTERED IS LESS THAN OR EQUAL TO ZER0 - JOKES ON THE USER, CANNOT HAPPEN
     if (isNaN(moneyIn)){
         alert('You Need to Enter a Valid Figure');
     } else {
         if (moneyIn <= 0){
-            alert('Very Funny, You Cannot Deposit Nothing or a Negative Figure, Can You?');
+            alert('Very Funny, You Cannot Deposit Nothing or a Negative Figure, or Can You?');
             document.getElementById('moneyin').value = '';
         } else {
             confirmMessage = 'Are You Sure You Want to Deposit ' + moneyIn + '?';
@@ -92,19 +116,24 @@ function deposit(){
 }
 
 function exit() {
+    // TRIGGERED WHEN EXIT BUTTON IS CLICKED
     confirmMessage = 'Are You Sure You Want to Exit The System?';
     downloadMessage = 'Do You Wish To Download Your Transactions?';
     if (window.confirm(confirmMessage)){
+        // IF THE USER WANT TO EXIT, THEY GET THE NEXT QUESTION ABOUT DOWNLOADING TRANSACTIONS
         if (window.confirm(downloadMessage)){
+            // HERE IS WHERE THE DOWNLOAD MAGIC HAPPENS
+            // WE NEED AN <a> ELEMENT TO CREATE A DOWNLOAD LINK
             let downloadLink = document.createElement("a");
-            let transactionNotes = atmTransactions.join('\n');
+            let transactionNotes = atmTransactions.join('\n'); // HERE WE PUT ALL THE TRANSACTIONS FROM THE ARRAY INTO A LIST
+            // NOW, WE USE A BLOB OBJECT TO CREATE A FILE
             downloadLink.href = window.URL.createObjectURL(new Blob([transactionNotes], {type: "text/plain"}));
             downloadLink.download = "transactionsReceipt.txt";
-            downloadLink.click();
+            downloadLink.click(); // AUTOMATICALLY CLICKS THE BUTTON TO DOWNLOAD FILE
             alert('Download Complete, Check Default Download Location for Transaction Text File.')
         }
         alert('SYSTEM CLOSING, LAST VISITED ON' + '\n' + today);
-        return loggedOut();
+        return loggedOut(); // GOES BACK TO MAIN PAGE
     }
 }
 
